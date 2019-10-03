b1a3c260ebbdc68a8a286b5615a26f3d51847dbe
Mikey Williams <manicdesigner@gmail.com>
2019-10-03 15:53:47 -0400

    onClick todoToggle ternary completed else ''

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import Styles from './Todo.css';
 
 function Todo(props) {
-  const { task } = props;
-  return <li className='completed'>{task.task}</li>;
+	const { task } = props;
+	return (
+		<li
+			onClick={props.todoToggle}
+			className={task.completed ? ' completed' : ''}
+		>
+			{task.task}
+		</li>
+	);
 }
 export default Todo;
